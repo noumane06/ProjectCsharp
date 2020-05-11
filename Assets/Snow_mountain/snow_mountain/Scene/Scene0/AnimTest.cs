abd43fedec9dfc9142f14e0b0300f8ca5855ddbc
noumane06 <noumane.06@gmail.com>
2020-05-11 22:22:14 +0000

    intro Environnement & Drone shot

---
+++ Assets/Snow_mountain/snow_mountain/Scene/Scene0/AnimTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timer = 0f;
+    
+    void Update()
+    {
+            timer += Time.deltaTime;
+            if (timer >= 5)
+            {
+            SceneManager.LoadScene(1);
+                
+            }
+            
+            
+    }
+        
+}
---
+++ Assets/Snow_mountain/snow_mountain/Scene/Scene1/CamerMovementScene1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerMovementScene1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timer = 0f;
+    public float speed;
+    public float rotSpeed;
+    public float posSpeed;
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer <= 10)
+        {
+
+                Vector3 pos = transform.position; // getting the position of the camera
+                transform.Rotate(-timer / 2 * rotSpeed, speed * timer * rotSpeed, 0);
+                pos.x += speed * timer * rotSpeed;
+                pos.y += speed * timer * rotSpeed;
+                pos.z += speed * timer * posSpeed;
+                transform.position = pos; // applying the new position 
+            
+        }
+
+    }
+}
